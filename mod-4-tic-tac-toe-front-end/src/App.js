bf03c0698771da132160e15cd4df3ca54b176810
Maximo Bautista <max.bautista97@gmail.com>
2019-04-15 15:51:41 -0400

    Created basic tic-tac-two game logic

---
+++ mod-4-tic-tac-toe-front-end/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Game from './components/Game'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Game  />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
